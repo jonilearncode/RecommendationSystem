fee8094db64d45f2d92f6e7e8c7fe34d463b6a4e
jonilearncode <jonilearncode@gmail.com>
2019-04-23 06:57:33 +0100

    Phase3 created, Refactoring, interface, HashMaps for efficiency.

--- Rater.java
+++ Rater.java
@@ -1,63 +1,18 @@
-/** 
- * AUTHOR
- * Capstone project for the "JAVA Programming and Software 
- * Enginnering Fundamentals" Specialization by Coursera/DukeUniversity.
- * @Author(jonilearncode)
- * @Version(05042019)
+
+/**
+ * Write a description of Rater here.
  * 
- * CLASS DESCRIPTION
- * This class keeps track of one rater and all their ratings.
- **/
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
 
 import java.util.*;
 
-public class Rater {
-    private String myID;
-    private ArrayList<Rating> myRatings;
-
-    public Rater(String id) {
-        myID = id;
-        myRatings = new ArrayList<Rating>();
-    }
-
-    public void addRating(String item, double rating) {
-        myRatings.add(new Rating(item,rating));
-    }
-
-    public boolean hasRating(String item) {
-        for(int k=0; k < myRatings.size(); k++){
-            if (myRatings.get(k).getItem().equals(item)){
-                return true;
-            }
-        }
-        
-        return false;
-    }
-
-    public String getID() {
-        return myID;
-    }
-
-    public double getRating(String item) {
-        for(int k=0; k < myRatings.size(); k++){
-            if (myRatings.get(k).getItem().equals(item)){
-                return myRatings.get(k).getValue();
-            }
-        }
-        
-        return -1;
-    }
-
-    public int numRatings() {
-        return myRatings.size();
-    }
-
-    public ArrayList<String> getItemsRated() {
-        ArrayList<String> list = new ArrayList<String>();
-        for(int k=0; k < myRatings.size(); k++){
-            list.add(myRatings.get(k).getItem());
-        }
-        
-        return list;
-    }
+public interface Rater {
+    public void addRating(String item, double rating);
+    public boolean hasRating(String item);
+    public String getID();
+    public double getRating(String item);
+    public int numRatings();
+    public ArrayList<String> getItemsRated();
 }
